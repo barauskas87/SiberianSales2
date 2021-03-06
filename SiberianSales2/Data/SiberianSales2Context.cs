0caabafd10cb30a3511c9538ded7cba8c93b38ef
barauskas87 <victor.barauskas@gmail.com>
2021-03-06 14:59:56 -0300

    CRUD Scaffolding

---
+++ SiberianSales2/Data/SiberianSales2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SiberianSales2.Models;
+
+namespace SiberianSales2.Data
+{
+    public class SiberianSales2Context : DbContext
+    {
+        public SiberianSales2Context (DbContextOptions<SiberianSales2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SiberianSales2.Models.Department> Department { get; set; }
+    }
+}
--- SiberianSales2/Startup.cs
+++ SiberianSales2/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using SiberianSales2.Data;
 
 namespace SiberianSales2
 {
@@ -33,6 +35,9 @@ namespace SiberianSales2
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SiberianSales2Context>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SiberianSales2Context")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
