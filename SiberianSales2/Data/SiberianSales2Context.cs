91ab794eb86942767a9786289481fa6bb97745ab
barauskas87 <victor.barauskas@gmail.com>
2021-03-27 16:33:06 -0300

    Revert "Other Entities and second migration"
    This reverts commit 35defb85b958a747d8d794d37f2e90b40fb06a0c.

--- SiberianSales2/Data/SiberianSales2Context.cs
+++ SiberianSales2/Data/SiberianSales2Context.cs
@@ -13,30 +13,7 @@ namespace SiberianSales2.Data
             : base(options)
         {
         }
-        public DbSet<Address> Address { get; set; }
-        public DbSet<Client> Client { get; set; }
-        public DbSet<ComissionDuplicate> ComissionDuplicate { get; set; }
-        public DbSet<Contact> Contact { get; set; }
-        public DbSet<Department> Department { get; set; }
-        public DbSet<Diary> Diary { get; set; }
-        public DbSet<FreightTypes> FreightTypes { get; set; }
-        public DbSet<Goals> Goals { get; set; }
-        public DbSet<PaymentDuplicate> PaymentDuplicate { get; set; }
-        public DbSet<PaymentForms> PaymentForms { get; set; }
-        public DbSet<Product> Product { get; set; }
-        public DbSet<ProductType> ProductType { get; set; }
-        public DbSet<ProposalItem> ProposalItem { get; set; }
-        public DbSet<PurchaseOrder> PurchaseOrder { get; set; }
-        public DbSet<PurchaseOrderItem> PurchaseOrderItem { get; set; }
-        public DbSet<Reseller> Reseller { get; set; }
-        public DbSet<SalesOrder> SalesOrder { get; set; }
-        public DbSet<SalesOrderItem> SalesOrderItem { get; set; }
-        public DbSet<SalesProposal> SalesProposal { get; set; }
-        public DbSet<Scheduling> Scheduling { get; set; }
-        public DbSet<Seller> Seller { get; set; }
-        public DbSet<Stock> Stock { get; set; }
-        public DbSet<Supplier> Supplier { get; set; }
-        public DbSet<TaxNumber> TaxNumber { get; set; }
 
+        public DbSet<SiberianSales2.Models.Department> Department { get; set; }
     }
 }
