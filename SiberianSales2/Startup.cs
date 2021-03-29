ab739829036ee159260795f328082edcc3023be6
barauskas87 <victor.barauskas@gmail.com>
2021-03-29 17:38:35 -0300

    SellerService and basic FindAll

--- SiberianSales2/Startup.cs
+++ SiberianSales2/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using SiberianSales2.Data;
+using SiberianSales2.Services;
 
 namespace SiberianSales2
 {
@@ -40,6 +41,7 @@ namespace SiberianSales2
                     options.UseMySql(Configuration.GetConnectionString("SiberianSales2Context"), builder => builder.MigrationsAssembly("SiberianSales2")));
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
